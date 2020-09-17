65e82b127d63a3bfc18323bed1e00eab32feac4d
RattyDAVE <dave@pucknell.co.uk>
2020-09-17 07:25:46 +0100

    Update start-training.sh

--- start-training.sh
+++ start-training.sh
@@ -23,11 +23,11 @@ Virtual_May19_Train_track
 
 "
 
-WORLD_NAME=reinvent_base
+#WORLD_NAME=reinvent_base
 WORLD_NAME=AWS_track
 
-sed -i 's/^\(WORLD_NAME\s*=\s*\).*$/WORLD_NAME=$WORLD_NAME/' config.env
-sed -i 's/^\(WORLD_NAME:\s* \s*\).*$/WORLD_NAME: "$WORLD_NAME"/' data/minio/bucket/custom_files/training_params.yaml
+sed -i "s/^\(WORLD_NAME\s*=\s*\).*$/WORLD_NAME=$WORLD_NAME/" config.env
+sed -i "s/^\(WORLD_NAME:\s* \s*\).*$/WORLD_NAME: \"$WORLD_NAME\"/" data/minio/bucket/custom_files/training_params.yaml
 
 if hash nvidia-docker 2>/dev/null; then
     echo "GPU Enabled"
