6dda0dbdf99fe182a1480e7dd589dc4ea2745e39
RattyDAVE <dave@pucknell.co.uk>
2020-09-18 10:58:24 +0100

    Update start-training.sh (#6)

--- start-training.sh
+++ start-training.sh
@@ -1,5 +1,35 @@
 #!/usr/bin/env bash
 
+#Changing Track does not work ...!
+#echo "List of supported worlds
+#(Taken from /opt/install/deepracer_simulation_environment/share/deepracer_simulation_environment/models)
+#
+#AWS_track                   bot_car
+#Albert                      box_obstacle
+#AmericasGeneratedInclStart  camera
+#Bowtie_track                reInvent 2019_track
+#Canada_Training             reInvent2019_wide
+#China_track                 reInvent2019_wide_mirrored
+#FS_June2020                 reinvent_base
+#July_2020                   reinvent_carpet
+#LGSWide                     reinvent_carpet_carpet
+#Mexico_track                reinvent_concrete
+#New_York_Track              reinvent_concrete_concrete
+#Oval_track                  reinvent_grass_asphalt
+#Spain_track                 reinvent_lines_walls
+#Straight_track              reinvent_wood
+#Tokyo_Training_track        reinvent_wood_wood
+#Vegas_track                 top_camera
+#Virtual_May19_Train_track
+#
+#"
+
+#WORLD_NAME=reinvent_base
+WORLD_NAME=July_2020
+
+sed -i "s/^\(WORLD_NAME\s*=\s*\).*$/WORLD_NAME=$WORLD_NAME/" config.env
+sed -i "s/^\(WORLD_NAME:\s* \s*\).*$/WORLD_NAME: \"$WORLD_NAME\"/" data/minio/bucket/custom_files/training_params.yaml
+
 if hash nvidia-docker 2>/dev/null; then
     echo "GPU Enabled"
     sed -i 's/^\(GPU_AVAILABLE\s*=\s*\).*$/GPU_AVAILABLE=True/' config.env
